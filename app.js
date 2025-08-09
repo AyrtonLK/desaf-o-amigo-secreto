854274d7ec750f3c2180f5f4bb39e784815d5cf7
AyrtonLK <ayrtonlkalmeidagmail.com>
2025-08-09 20:20:13 -0300

    funcion-para-agregar

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+//voy a tener el valor dentro del input
+let input = document.getElementById("amigo");
+let nombre = input.value.trim(); // para eliminar los espacios en el inicio y final
+
+//acá verifico que no este vacio
+if (nombre == "") {
+    alert('Por favor, escribe un nombre.');
+    return;
+}
+
+// Convertir primera letra en mayúscula y resto en minúsculas
+    nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+
+//lo guardo adentro del array
+listaAmigos.push(nombre);
+
+console.log(listaAmigos);
+
+//limpieza del input para que se escriban otros nombres
+input.value = "";
+
+}
