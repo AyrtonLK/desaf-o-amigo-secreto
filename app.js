df5153dbb855c4587bbe8192bd7dabde756fca60
AyrtonLK <ayrtonlkalmeidagmail.com>
2025-08-13 19:06:05 -0300

    agrego-funcoion-sortearAmigo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let listaAmigos = [];
+let amigos = [];
 
 function agregarAmigo() {
 //voy a tener el valor dentro del input
@@ -11,15 +11,50 @@ if (nombre == "") {
     return;
 }
 
-// Convertir primera letra en mayúscula y resto en minúsculas
+//convertir primera letra en mayúscula y resto en minúsculas
     nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
 
 //lo guardo adentro del array
-listaAmigos.push(nombre);
+amigos.push(nombre);
 
-console.log(listaAmigos);
+console.log(amigos);
 
 //limpieza del input para que se escriban otros nombres
 input.value = "";
 
+//actualizo la lista en pantalla
+    mostrarLista();
+
+}
+
+function mostrarLista() {
+    //obtengo el elemento ul
+    let listaAmigos = document.getElementById('listaAmigos');   
+
+    //limpio el contenido
+    listaAmigos.innerHTML = "";
+
+    //uso el for para recorrer el array y agregar <li> para cada nombre
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+
+}
+
+function sortearAmigo() {
+    //validar que haya amigos en el array
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    //generar un índice aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    //obtener el nombre sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    //mostrar el resultado en la lista de resultado
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
 }
